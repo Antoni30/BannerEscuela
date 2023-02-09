513d15648e69163caee1093e27ac085a0655637b
Antoni30 <Antoni30@github.com>
2023-02-09 01:56:58 -0500

    nuevoProyecto

---
+++ Cliente/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Boton from './Components/Boton'
+
+export default function App() {
+  return (
+    <>
+        <Boton name='Ingresar'/>
+    </>
+  )
+}
---
+++ Cliente/src/Components/Boton.jsx
@@ -0,0 +1,7 @@
+export default function Boton({ name }) {
+  return (
+    <button className="border-solid border-2 border-gray-400 w-40 h-10 font-thin rounded-full text-gray-500 hover:bg-gray-600 hover:text-white">
+      {name}
+    </button>
+  );
+}
